fc8e2e3e43c87eeabc6f2fb0ee56f9fd8cbc5bb0
Vishal Naik <vishalnaik@thoughtworks.com>
2015-12-07 12:57:59 -0300

    added link to coverage report

--- upload_reports_to_s3.sh
+++ upload_reports_to_s3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 aws s3 sync target/site s3://snap-build-reports/snap-ci-examples/spring-petclinic/${SNAP_PIPELINE_COUNTER} --acl private --region us-west-2 --delete
-echo "Click here for build reports https://s3-browser-example.herokuapp.com/list/snap-ci-examples/spring-petclinic/$SNAP_PIPELINE_COUNTER/"
+echo "Build reports uploaded to: https://s3-browser-example.herokuapp.com/list/snap-ci-examples/spring-petclinic/$SNAP_PIPELINE_COUNTER/"
+echo "Code coverage report: https://s3-browser-example.herokuapp.com/proxy/snap-ci-examples/spring-petclinic/$SNAP_PIPELINE_COUNTER/cobertura/index.html"
