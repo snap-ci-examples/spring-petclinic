5cb4f5adff6004d62a1e8313ea8d24edf3bb6b3d
Vishal Naik <vishalnaik@thoughtworks.com>
2015-12-07 10:43:39 -0300

    upload build reports to s3

---
+++ upload_reports_to_s3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+aws s3 sync target/site s3://snap-build-reports/snap-ci-examples/spring-petclinic/${SNAP_PIPELINE_COUNTER} --acl private --region us-west-2 --delete
+echo 'Click here for build reports https://s3-browser-example.herokuapp.com/list/snap-ci-examples/spring-petclinic/${SNAP_PIPELINE_COUNTER}/'
